293ebb1bf0e6415e874b2bd2647ddf814fc9de59
Jessica <jessicakchell@gmail.com>
2021-10-23 11:06:34 +0200

    deleted extra code

--- js/libs/filteringArray.js
+++
@@ -1,9 +0,0 @@
-export const filteringAnArray = (array, filterString) => {
-	return array.filter((arrayElement) => {
-		if (arrayElement.title.toLowerCase().includes(filterString)) {
-			return true;
-		}
-	});
-};
-
-// delete this file if not using as import
